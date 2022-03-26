1f1e10470028556391c85008cb6de42fa2fd1ec6
Ralf S. Engelschall <rse@engelschall.com>
2022-03-26 18:03:19 +0100

    update year in copyright messages

--- hostid-cli.js
+++ hostid-cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*!
 **  HostId -- Determine Universally Unique Identifier of Host
-**  Copyright (c) 2016-2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2016-2022 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
